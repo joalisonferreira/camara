5cd8c6e56bc084bac79d3f012c6c568bddb12330
Joalison <joaliso1@pro106.dnspro.com.br>
2025-04-28 13:24:58 -0300

    commit inicial

---
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash680fa91b053c5/kmo3YG_Dkxhu4VZazvaPJI3Ej1SbFayd9NDEeZkHaw8.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash680fa91b053c5 extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
